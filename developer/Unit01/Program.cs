aedbe74a2c6751c716d98481d387b2dbc3f730cd
Lora Chisholm <chi20006@byui.edu>
2022-09-27 17:19:04 -0600

    new line

--- developer/Unit01/Program.cs
+++ developer/Unit01/Program.cs
@@ -8,10 +8,24 @@ namespace Unit01
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Tic Tac Toe game!");
+            Console.WriteLine("This is a new line");
+            // int[]gameboard = new int[9];
+            // board[0] = 0;
+            // board[1] = 1;
+            // board[2] = 2;
+            // board[3] = 3;
+            // board[4] = 4;
+            // board[5] = 5;
+            // board[6] = 6;
+            // board[7] = 7;
+            // board[8] = 8;
+            // board[9] = 9;
+
+            //for (int i = 0; i < 9: i++)
+            //{
 
 
-
-
+            //}
         }
     }
 }
